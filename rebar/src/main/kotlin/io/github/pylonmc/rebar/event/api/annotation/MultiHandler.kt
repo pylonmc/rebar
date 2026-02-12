79313bc53bf9d655ce09fa3c90a091cbfff237c9
JustAHuman-xD <imjustahuman.xd@gmail.com>
2026-02-12 13:02:29 -0600

    actually support inheritance lol

--- rebar/src/main/kotlin/io/github/pylonmc/rebar/event/api/annotation/MultiHandler.kt
+++ rebar/src/main/kotlin/io/github/pylonmc/rebar/event/api/annotation/MultiHandler.kt
@@ -4,7 +4,9 @@ import org.bukkit.event.Cancellable
 import org.bukkit.event.Event
 import org.bukkit.event.EventPriority
 import java.lang.invoke.MethodHandles
+import java.lang.reflect.Method
 import java.lang.reflect.Modifier
+import kotlin.collections.getOrPut
 import kotlin.collections.toSet
 
 annotation class MultiHandler(
@@ -31,15 +33,8 @@ annotation class MultiHandler(
             val eventClass = event::class.java
             val info = EventInfo(handlerMethod, eventClass)
             val function = handlerMap.getOrPut(info) {
-                val method = handler::class.java.declaredMethods.firstOrNull { it.name == info.handlerMethod }
-                if (method == null) {
-                    return@getOrPut { _, _, _, -> {} }
-                }
-                if (method.parameters.size != 2 || info.eventClass != method.parameters[0].type || method.parameters[1].type != EventPriority::class.java) {
-                    throw IllegalStateException("Method ${method.name} in class ${directClass.name} must have exactly two parameters, the first being ${eventClass}, and the second being EventPriority")
-                } else if (Modifier.isAbstract(method.modifiers)) {
-                    throw IllegalStateException("Method ${method.name} in class ${directClass.name} must not be abstract")
-                } else if (!method.trySetAccessible()) {
+                val method = findMethod(directClass, info)
+                if (!method.trySetAccessible()) {
                     throw IllegalStateException("Could not access method ${method.name} in class ${directClass.name}")
                 }
 
@@ -61,6 +56,32 @@ annotation class MultiHandler(
                 }
             }
             function(handler, event, priority)
+        }
+
+        internal fun findMethod(clazz: Class<*>, info: EventInfo) : Method {
+            return try {
+                clazz.declaredMethods.firstOrNull {
+                    it.parameters.size == 2
+                            && it.parameters[0].type == info.eventClass
+                            && it.parameters[1].type == EventPriority::class.java
+                            && it.name == info.handlerMethod
+                            && !Modifier.isAbstract(it.modifiers)
+                } ?: throw NoSuchMethodException("Could not find method $info.handlerMethod in class ${clazz.name} with parameters (${info.eventClass.name}, EventPriority)")
+            } catch (_: NoSuchMethodException) {
+                for (interfaceClass in clazz.interfaces) {
+                    try {
+                        return findMethod(interfaceClass, info)
+                    } catch (_: NoSuchMethodException) {
+                        // ignore and continue searching
+                    }
+                }
+
+                if (clazz.superclass != null) {
+                    findMethod(clazz.superclass, info)
+                } else {
+                    throw NoSuchMethodException("Could not find method $info.handlerMethod in class ${clazz.name} or its superclasses")
+                }
+            }
         }
 
         internal data class EventInfo(
