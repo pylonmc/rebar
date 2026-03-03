e495ec8098b60f5dfc7eea5726869dadcd65f084
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-03-03 15:54:12 -0500

    format

--- rebar/src/main/kotlin/io/github/pylonmc/rebar/config/adapter/TextColorConfigAdapter.kt
+++ rebar/src/main/kotlin/io/github/pylonmc/rebar/config/adapter/TextColorConfigAdapter.kt
@@ -8,9 +8,9 @@ object TextColorConfigAdapter : ConfigAdapter<TextColor> {
     override val type: Type = TextColor::class.java
 
     override fun convert(value: Any): TextColor {
-        if(value is String){
+        if (value is String) {
             val color = TextColor.fromHexString("#${value.trimStart().trimEnd()}")
-            if(color != null){
+            if (color != null) {
                 return color
             } else {
                 throw RuntimeException("Unable to convert hex color #${value.trimStart().trimEnd()} to TextColor")
@@ -20,13 +20,13 @@ object TextColorConfigAdapter : ConfigAdapter<TextColor> {
             val h = section.get("h", ConfigAdapter.FLOAT)
             val s = section.get("s", ConfigAdapter.FLOAT)
             val v = section.get("v", ConfigAdapter.FLOAT)
-            if(h != null && s != null && v != null){
+            if (h != null && s != null && v != null) {
                 return TextColor.color(HSVLike.hsvLike(h, s, v))
             }
             val r = section.get("r", ConfigAdapter.INTEGER)
             val g = section.get("g", ConfigAdapter.INTEGER)
             val b = section.get("b", ConfigAdapter.INTEGER)
-            if(r != null && g != null && b != null && v != null){
+            if (r != null && g != null && b != null && v != null) {
                 return TextColor.color(r, g, b)
             }
             throw RuntimeException("Could not find hsv or rgb values in TextColor config section")
